15069d0aa65167c82dcd215c16bba66903c5ce1a
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2021-09-01 13:34:44 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,5 @@
+print('hello world') # output to terminal. Command: python hello.py
+name = input('what is your name: ') # input from terminal
+print('its nice to meet you ' + name)
+age = input('how old are you?: ')
+print('wow, ' + age + ' You are very old')
