e768e7b5be6b4252762c2d81d8f54b72f6caeb8e
AmarNagim <99069493@mydavinci.nl>
2021-09-01 13:41:44 +0200

    V2.0

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ name = input('what is your name: ') # input from terminal
 print('its nice to meet you ' + name)
 age = input('how old are you?: ')
 print('wow, ' + age + ' You are very old')
+
+# this is a test
