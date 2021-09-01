f18fc147edd02acc8b7ad3b94a59d36b8162284e
AmarNagim <99069493@mydavinci.nl>
2021-09-01 14:06:07 +0200

    V3.0

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
+import time
+
 print('hello world') # output to terminal. Command: python hello.py
 name = input('what is your name: ') # input from terminal
 print('its nice to meet you ' + name)
 age = input('how old are you?: ')
-print('wow, ' + age + ' You are very old')
+print('wow, ' + age + ', You are very old!')
+time.sleep(2)
+dev_since = input('What year did you start developing? ')
+since = 2021 - int(dev_since)
+print('Oh, cool! You have been developing for '+ str(since) + ' years.')
 
 # this is a test
